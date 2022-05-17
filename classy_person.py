660d33a6eaee148c1e9a9208c44fd856f7974215
Nathan Gearke <Nathan.Gearke@veteransunited.com>
2022-05-17 18:30:06 -0500

    test

--- classy_person.py
+++ classy_person.py
@@ -6,8 +6,20 @@ don't forget the use of 'self' and to have the methods:
 3. say_greeting
 4. count_to_age
 '''
-
 class Person:
+    def __init__(self, age, name):
+        self.age = age
+        self.name = name
+
+    def increase_age(self):
+        self.age = self.age + 1
+
+    def say_greeting(self):
+        print(f"Hello world! My name is {self.name}!")
+
+    def count_to_age(self):
+        for year in range(1, self.age + 1):
+            print(year)
 
 
 
